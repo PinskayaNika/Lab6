fdc4b43b4f0c45b7f6203d0dde34f779a38c31d2
nika <pinskaya.nika@mail.ru>
2019-11-30 01:14:32 +0300

    Scripted auto-commit on change (2019-11-30 01:14:32) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/StoreActor.java
+++ lab6/src/main/java/com/examples/akkahttpserver/StoreActor.java
@@ -1,4 +1,45 @@
 package com.examples.akkahttpserver;
 
-public class StoreActor {
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.japi.pf.ReceiveBuilder;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StoreActor extends AbstractActor {
+    private HashMap<String, Map<Integer, Integer>> data = new HashMap<>();
+
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+
+                //принимает поиск уже готового результата тестирования
+                .match(FindingResult.class, msg -> {
+                            String url = msg.getURL();
+                            int count = msg.getCount();
+                            if (data.containsKey(url) && data.get(url).containsKey(count)) {
+                                getSender().tell(
+                                        data.get(url).get(count),
+                                        ActorRef.noSender());
+                            } else {
+                                getSender().tell(-1, ActorRef.noSender());
+                            }
+                        }
+                )
+
+                //принимает результат тестрования
+                .match(TestingResult.class, msg -> {
+                            Map<Integer, Integer> temp;
+                            if (data.containsKey(msg.getURL())) {
+                                temp = data.get(msg.getURL());
+                            } else {
+                                temp = new HashMap<>();
+                            }
+                            temp.put(msg.getCount(), msg.getTime());
+                            data.put(msg.getURL(), temp);
+
+                        }
+                ).build();
+    }
 }
