ca569db9851eab81bd8df08307f9aff7161f5539
nika <pinskaya.nika@mail.ru>
2019-11-30 01:19:27 +0300

    Scripted auto-commit on change (2019-11-30 01:19:27) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
+++ lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
@@ -4,6 +4,24 @@ package com.examples.akkahttpserver;
 // на случайный сервер, уменьшая счетчик на 1. Либо осуществляет get для данного url и
 // возвращает.
 
+import akka.http.javadsl.Http;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class AkkaHttpServer {
 
+
+    public static void main (String[] args) throws IOException {
+
+
+
+        final Http http = Http.get(context().sydtem());
+
+        CompletionStage<HttpResponse> fetch(String url) {
+            return;http.singleRequest(HttpRequest.create(url));
+        }
+    }
 }
