1d4e8590a1abf9bef769bc97a83bca24e267d64a
nika <pinskaya.nika@mail.ru>
2019-11-30 01:15:02 +0300

    Scripted auto-commit on change (2019-11-30 01:15:02) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/StoreActor.java
+++ lab6/src/main/java/com/examples/akkahttpserver/StoreActor.java
@@ -1,5 +1,10 @@
 package com.examples.akkahttpserver;
 
+//а. создаем актор хранилище конфигурации.
+//Он принимает две команды —
+//-	список серверов (который отправит zookeeper watcher)
+//-	запрос на получение случайного сервера
+
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.japi.pf.ReceiveBuilder;
