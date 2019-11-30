2c50cff11e507d6dee9bf40458069184aa7df76b
nika <pinskaya.nika@mail.ru>
2019-11-30 10:05:02 +0300

    Scripted auto-commit on change (2019-11-30 10:05:02) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
+++ lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
@@ -7,21 +7,46 @@ package com.examples.akkahttpserver;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletionStage;
 
 public class AkkaHttpServer {
+    private static ZooKeeper zooKeeper;
+
+    public static void main (String[] args) throws IOException, KeeperException, InterruptedException {
+
+// подключение к зукиперу внутри программы
+        zooKeeper = new ZooKeeper(
+                "127.0.0.1:2181",
+                2000,
+                a -> {}
+                );
+// zapuskaew odin raz potom kommentiw
+        zooKeeper.create("/servers", "server".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        zooKeeper.create("/servers/" + PORT_NUMBER, PORT_NUMBER.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+        zooKeeper.getChildren("/servers", a -> {
+            // tut mi poluchaem dannie o tekuwix serverax
+            List<String> servers = new ArrayList<>();
+            try {
+                servers = zooKeeper.getChildren("/servers", b -> {});
+            } catch (KeeperException e) {
 
 
-    public static void main (String[] args) throws IOException {
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
 
-
-
-        final Http http = Http.get(context().system());
-
-        CompletionStage<HttpResponse> fetch(String url) {
-            return;http.singleRequest(HttpRequest.create(url));
-        }
+            for(String s: servers){
+                System.out.print(zooKeeper.getData("/servers" + s, c -> {}, null).toString());
+            }
+        });
     }
 }
