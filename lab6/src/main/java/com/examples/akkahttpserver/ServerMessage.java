6e934db0f5f7cdf2218511f48ad6ac0ac5a5bc8f
nika <pinskaya.nika@mail.ru>
2019-12-02 11:04:08 +0300

    Scripted auto-commit on change (2019-12-02 11:04:08) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/ServerMessage.java
+++ lab6/src/main/java/com/examples/akkahttpserver/ServerMessage.java
@@ -1,4 +1,15 @@
 package com.examples.akkahttpserver;
 
+import java.util.List;
+
 public class ServerMessage {
+    private List<String> serverList;
+
+    public ServerMessage(List<String> port) {
+        this.serverList = port;
+    }
+
+    public List<String> getServerPort() {
+        return this.serverList;
+    }
 }
