cc46d92f43675b1b726639e78d5757f18b007788
nika <pinskaya.nika@mail.ru>
2019-12-06 20:03:55 +0300

    Scripted auto-commit on change (2019-12-06 20:03:55) by gitwatch.sh

--- lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
+++ lab6/src/main/java/com/examples/akkahttpserver/AkkaHttpServer.java
@@ -124,6 +124,16 @@ public class AkkaHttpServer extends AllDirectives {
         }
     }
 
+    CompletionStage<HttpResponse> fetchToServer (int port, String url, int parsedCount) {
+        try {
+            return http.singleRequest(
+                    HttpRequest.create("http://localhost:" + Integer.toString(port) + "/?url=" + url + "&count=" +
+                            Integer.toString(parsedCount - 1)));
+        } catch (Exception e) {
+            return CompletableFuture.completedFuture(HttpResponse.create().withEntity(NOT_FOUND));
+        }
+    }
+
     private Route route() {
         return concat(
                 get(
